d3f9b45c81ec61021918522939410f953f9732f4
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-10 15:43:40 +0600

    creating dropdown

--- dropdown.php
+++ dropdown.php
@@ -1,17 +1,44 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
+    <form method="post">
+        <label for="courseName">Name : </label>
+        <input type="text" name="courseName"/>
+    </form>
+        
     </head>
     <body>
         <?php
-        // put your code here
+         require_once 'dbconnection.php';                
+            
+            
+        
+        $courses = array();
+        
+        $dbConnection = new DbConnection();
+        
+        $con = $dbConnection->getConnection(); 
+        
+        mysql_select_db('ftfl');
+        
+        $query =mysql_query("select title from courses");
+        
+        echo 'Course Name : ';
+        
+        echo '<select>';
+               
+        while ( $data = mysql_fetch_object($query))
+        {
+            echo "<option>".$data->title."</option>";
+           
+        }        
+        echo '<select>';
+         
+        //}
         ?>
     </body>
 </html>
