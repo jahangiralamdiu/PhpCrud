5a2a9582d94921325e529ae594153e1224dcaecf
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-14 16:59:42 +0600

    add php include

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="well">
+            <p>@Copy Right Information. Developed by Md. Jahangir Alam</p>                     
+        </div>
+    </div>
+</div>
--- student/studentregistration.php
+++ student/studentregistration.php
@@ -24,72 +24,9 @@ and open the template in the editor.
     <body>
         <div class="container-fluid">
 
-            <div class="row">
-                <div class="col-mid-12">
-                    <div class="page-header">
-                        <h1>Student Management</h1>
-                    </div>
-                </div>
-            </div>
+             <?php include '../header.php';
+            ?>
             
-            <div class="row">
-
-                <nav class="navbar navbar-default" role="navigation">
-
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand glyphicon glyphicon glyphicon-th" href="#"></a>
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            
-                            <li><a href="http://localhost/PhpCrud/dashboard.php"><span class="glyphicon glyphicon-home"> Home</span></a></li>
-                            
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Student <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="http://localhost/PhpCrud/student/index.php">Students</a></li>
-                                    <li><a href="http://localhost/PhpCrud/student/studentregistration.php">Add New</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="http://localhost/PhpCrud/student/studentregistration.php">View</a></li>                                
-                                    
-                                </ul>
-                            </li>                          
-                             
-                            
-                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Course<span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="http://localhost/PhpCrud/course/index.php">courses</a></li>
-                                    <li><a href="http://localhost/PhpCrud/course/courseregistration.php">Add New</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="http://localhost/PhpCrud/course/courseregistration.php">View</a></li>                                
-                                    
-                                </ul>
-                            </li>
-                            
-                            <li><a href="#">About Me</a></li> 
-                        </ul>
-                        <form class="navbar-form navbar-right" role="search">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                            <button type="submit" class="btn btn-default glyphicon glyphicon-search"></button>
-                        </form>
-                        
-                    </div><!-- /.navbar-collapse -->
-
-                </nav>
-
-            </div>
 
             <div class="row">
 
@@ -141,16 +78,9 @@ and open the template in the editor.
                 </div>
 
             </div> 
-            
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="well">
-                        <p>@Copy Right Information. Developed by Md. Jahangir Alam</p>                     
-                    </div>
-                </div>
-            </div>
 
-        </div>
+            <?php include '../footer.php';
+            ?>
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
