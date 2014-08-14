f8528f61920736f69c239469a5c4275796a529f0
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-14 13:22:07 +0600

    add manubar to all

--- course/index.php
+++ course/index.php
@@ -31,6 +31,65 @@ and open the template in the editor.
                     </div>
                 </div>
             </div>
+            
+            <div class="row">
+
+                <nav class="navbar navbar-default" role="navigation">
+
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand glyphicon glyphicon glyphicon-th" href="#"></a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            
+                            <li><a href="http://localhost/PhpCrud/dashboard.php"><span class="glyphicon glyphicon-home"> Home</span></a></li>
+                            
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Student <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="http://localhost/PhpCrud/student/index.php">Students</a></li>
+                                    <li><a href="http://localhost/PhpCrud/student/studentregistration.php">Add New</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="http://localhost/PhpCrud/student/studentregistration.php">View</a></li>                                
+                                    
+                                </ul>
+                            </li>                          
+                             
+                            
+                             <li class="dropdown">
+                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Course<span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="http://localhost/PhpCrud/course/index.php">courses</a></li>
+                                    <li><a href="http://localhost/PhpCrud/course/courseregistration.php">Add New</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="http://localhost/PhpCrud/course/courseregistration.php">View</a></li>                                
+                                    
+                                </ul>
+                            </li>
+                            
+                            <li><a href="#">About Me</a></li> 
+                        </ul>
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default glyphicon glyphicon-search"></button>
+                        </form>
+                        
+                    </div><!-- /.navbar-collapse -->
+
+                </nav>
+
+            </div>
 
             <div class="row">
 
