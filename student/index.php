b370b793fbb2b2ef6033b9ca5405a776e0f55c4b
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-14 10:43:56 +0600

    applying bootstrap on student home page

--- student/index.php
+++ student/index.php
@@ -9,10 +9,10 @@ and open the template in the editor.
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Student</title>
+        <title>Student edit update delete</title>
 
         <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -27,7 +27,7 @@ and open the template in the editor.
             <div class="row">
                 <div class="col-mid-12">
                     <div class="page-header">
-                        <h1>Student Management Dashboard</h1>
+                        <h1>Student Management</h1>
                     </div>
                 </div>
             </div>
@@ -51,10 +51,72 @@ and open the template in the editor.
                     <div class="row">
 
                         <div class="col-md-12">
-                            <div class="jumbotron">                           
-                                <h3>Registered Students</h3>
-                                <p><a class="btn btn-primary btn-lg" role="button">View details</a></p>
+
+                            <h3>Students</h3>
+
+                            <div class="table-responsive">
+                                
+                                <table class="table">
+                                    <thead>
+                                    <th>ID </th>
+                                    <th>Name</th>
+                                    <th>View</th>
+                                    <th>Update</th>
+                                    <th>Delete</th>
+                                    <th>Assign Course</th>
+                                    </thead>
+                                    
+                                    <tbody>
+                                        <tr>
+                                            <td>1001</td>
+                                            <td>Jahangir</td>
+                                            <td><a>View</a></td>
+                                            <td><a href="#">Update</a></td>
+                                            <td><a class="alert-link" href="#">Delete</a></td>
+                                            <td><a class="alert-link" href="#">Assign Course</a></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>1002</td>
+                                            <td>Abrar</td>
+                                            <td><a>View</a></td>
+                                            <td><a href="#">Update</a></td>
+                                            <td><a class="alert-link" href="#">Delete</a></td>
+                                            <td><a class="alert-link" href="#">Assign Course</a></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>1003</td>
+                                            <td>Tanjim</td>
+                                            <td><a>View</a></td>
+                                            <td><a href="#">Update</a></td>
+                                            <td><a class="alert-link" href="#">Delete</a></td>
+                                            <td><a class="alert-link" href="#">Assign Course</a></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>1004</td>
+                                            <td>Toufiq</td>
+                                            <td><a>View</a></td>
+                                            <td><a href="#">Update</a></td>
+                                            <td><a class="alert-link" href="#">Delete</a></td>
+                                            <td><a class="alert-link" href="#">Assign Course</a></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>1005</td>
+                                            <td>Ishrat</td>
+                                            <td><a>View</a></td>
+                                            <td><a href="#">Update</a></td>
+                                            <td><a class="alert-link" href="#">Delete</a></td>
+                                            <td><a class="alert-link" href="#">Assign Course</a></td>
+                                        </tr>
+                                        
+                                    </tbody>
+                                    
+                                </table>
                             </div>
+
                         </div>
 
                     </div>                    
@@ -68,6 +130,6 @@ and open the template in the editor.
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
